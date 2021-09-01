f250d7b03ee120c1a444280f99d5e3d1af4eb8ea
witcher-vae <dhuuloc8818@gmail.com>
2021-09-02 02:29:08 +0700

    add GroupMember entity for answerService

---
+++ AnswerStore/src/main/java/vn/vnedu/studyspace/answer_store/repository/GroupMemberRepositoryInternalImpl.java
@@ -0,0 +1,143 @@
+package vn.vnedu.studyspace.answer_store.repository;
+
+import static org.springframework.data.relational.core.query.Criteria.where;
+import static org.springframework.data.relational.core.query.Query.query;
+
+import io.r2dbc.spi.Row;
+import io.r2dbc.spi.RowMetadata;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import org.springframework.data.relational.core.query.Criteria;
+import org.springframework.data.relational.core.sql.Column;
+import org.springframework.data.relational.core.sql.Expression;
+import org.springframework.data.relational.core.sql.Select;
+import org.springframework.data.relational.core.sql.SelectBuilder.SelectFromAndJoin;
+import org.springframework.data.relational.core.sql.Table;
+import org.springframework.r2dbc.core.DatabaseClient;
+import org.springframework.r2dbc.core.RowsFetchSpec;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import vn.vnedu.studyspace.answer_store.domain.GroupMember;
+import vn.vnedu.studyspace.answer_store.repository.rowmapper.GroupMemberRowMapper;
+import vn.vnedu.studyspace.answer_store.service.EntityManager;
+
+/**
+ * Spring Data SQL reactive custom repository implementation for the GroupMember entity.
+ */
+@SuppressWarnings("unused")
+class GroupMemberRepositoryInternalImpl implements GroupMemberRepositoryInternal {
+
+    private final DatabaseClient db;
+    private final R2dbcEntityTemplate r2dbcEntityTemplate;
+    private final EntityManager entityManager;
+
+    private final GroupMemberRowMapper groupmemberMapper;
+
+    private static final Table entityTable = Table.aliased("group_member", EntityManager.ENTITY_ALIAS);
+
+    public GroupMemberRepositoryInternalImpl(
+        R2dbcEntityTemplate template,
+        EntityManager entityManager,
+        GroupMemberRowMapper groupmemberMapper
+    ) {
+        this.db = template.getDatabaseClient();
+        this.r2dbcEntityTemplate = template;
+        this.entityManager = entityManager;
+        this.groupmemberMapper = groupmemberMapper;
+    }
+
+    @Override
+    public Flux<GroupMember> findAllBy(Pageable pageable) {
+        return findAllBy(pageable, null);
+    }
+
+    @Override
+    public Flux<GroupMember> findAllBy(Pageable pageable, Criteria criteria) {
+        return createQuery(pageable, criteria).all();
+    }
+
+    RowsFetchSpec<GroupMember> createQuery(Pageable pageable, Criteria criteria) {
+        List<Expression> columns = GroupMemberSqlHelper.getColumns(entityTable, EntityManager.ENTITY_ALIAS);
+        SelectFromAndJoin selectFrom = Select.builder().select(columns).from(entityTable);
+
+        String select = entityManager.createSelect(selectFrom, GroupMember.class, pageable, criteria);
+        String alias = entityTable.getReferenceName().getReference();
+        String selectWhere = Optional
+            .ofNullable(criteria)
+            .map(
+                crit ->
+                    new StringBuilder(select)
+                        .append(" ")
+                        .append("WHERE")
+                        .append(" ")
+                        .append(alias)
+                        .append(".")
+                        .append(crit.toString())
+                        .toString()
+            )
+            .orElse(select); // TODO remove once https://github.com/spring-projects/spring-data-jdbc/issues/907 will be fixed
+        return db.sql(selectWhere).map(this::process);
+    }
+
+    @Override
+    public Flux<GroupMember> findAll() {
+        return findAllBy(null, null);
+    }
+
+    @Override
+    public Mono<GroupMember> findById(Long id) {
+        return createQuery(null, where("id").is(id)).one();
+    }
+
+    private GroupMember process(Row row, RowMetadata metadata) {
+        GroupMember entity = groupmemberMapper.apply(row, "e");
+        return entity;
+    }
+
+    @Override
+    public <S extends GroupMember> Mono<S> insert(S entity) {
+        return entityManager.insert(entity);
+    }
+
+    @Override
+    public <S extends GroupMember> Mono<S> save(S entity) {
+        if (entity.getId() == null) {
+            return insert(entity);
+        } else {
+            return update(entity)
+                .map(
+                    numberOfUpdates -> {
+                        if (numberOfUpdates.intValue() <= 0) {
+                            throw new IllegalStateException("Unable to update GroupMember with id = " + entity.getId());
+                        }
+                        return entity;
+                    }
+                );
+        }
+    }
+
+    @Override
+    public Mono<Integer> update(GroupMember entity) {
+        //fixme is this the proper way?
+        return r2dbcEntityTemplate.update(entity).thenReturn(1);
+    }
+}
+
+class GroupMemberSqlHelper {
+
+    static List<Expression> getColumns(Table table, String columnPrefix) {
+        List<Expression> columns = new ArrayList<>();
+        columns.add(Column.aliased("id", table, columnPrefix + "_id"));
+        columns.add(Column.aliased("user_login", table, columnPrefix + "_user_login"));
+        columns.add(Column.aliased("group_id", table, columnPrefix + "_group_id"));
+        columns.add(Column.aliased("role", table, columnPrefix + "_role"));
+
+        return columns;
+    }
+}
---
+++ AnswerStore/src/main/java/vn/vnedu/studyspace/answer_store/service/GroupMemberService.java
@@ -0,0 +1,110 @@
+package vn.vnedu.studyspace.answer_store.service;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import vn.vnedu.studyspace.answer_store.domain.GroupMember;
+import vn.vnedu.studyspace.answer_store.repository.GroupMemberRepository;
+import vn.vnedu.studyspace.answer_store.service.dto.GroupMemberDTO;
+import vn.vnedu.studyspace.answer_store.service.mapper.GroupMemberMapper;
+
+/**
+ * Service Implementation for managing {@link GroupMember}.
+ */
+@Service
+@Transactional
+public class GroupMemberService {
+
+    private final Logger log = LoggerFactory.getLogger(GroupMemberService.class);
+
+    private final GroupMemberRepository groupMemberRepository;
+
+    private final GroupMemberMapper groupMemberMapper;
+
+    public GroupMemberService(GroupMemberRepository groupMemberRepository, GroupMemberMapper groupMemberMapper) {
+        this.groupMemberRepository = groupMemberRepository;
+        this.groupMemberMapper = groupMemberMapper;
+    }
+
+    /**
+     * Save a groupMember.
+     *
+     * @param groupMemberDTO the entity to save.
+     * @return the persisted entity.
+     */
+    public Mono<GroupMemberDTO> save(GroupMemberDTO groupMemberDTO) {
+        log.debug("Request to save GroupMember : {}", groupMemberDTO);
+        return groupMemberRepository.save(groupMemberMapper.toEntity(groupMemberDTO)).map(groupMemberMapper::toDto);
+    }
+
+    /**
+     * Partially update a groupMember.
+     *
+     * @param groupMemberDTO the entity to update partially.
+     * @return the persisted entity.
+     */
+    public Mono<GroupMemberDTO> partialUpdate(GroupMemberDTO groupMemberDTO) {
+        log.debug("Request to partially update GroupMember : {}", groupMemberDTO);
+
+        return groupMemberRepository
+            .findById(groupMemberDTO.getId())
+            .map(
+                existingGroupMember -> {
+                    groupMemberMapper.partialUpdate(existingGroupMember, groupMemberDTO);
+
+                    return existingGroupMember;
+                }
+            )
+            .flatMap(groupMemberRepository::save)
+            .map(groupMemberMapper::toDto);
+    }
+
+    /**
+     * Get all the groupMembers.
+     *
+     * @return the list of entities.
+     */
+    @Transactional(readOnly = true)
+    public Flux<GroupMemberDTO> findAll() {
+        log.debug("Request to get all GroupMembers");
+        return groupMemberRepository.findAll().map(groupMemberMapper::toDto);
+    }
+
+    /**
+     * Returns the number of groupMembers available.
+     * @return the number of entities in the database.
+     *
+     */
+    public Mono<Long> countAll() {
+        return groupMemberRepository.count();
+    }
+
+    /**
+     * Get one groupMember by id.
+     *
+     * @param id the id of the entity.
+     * @return the entity.
+     */
+    @Transactional(readOnly = true)
+    public Mono<GroupMemberDTO> findOne(Long id) {
+        log.debug("Request to get GroupMember : {}", id);
+        return groupMemberRepository.findById(id).map(groupMemberMapper::toDto);
+    }
+
+    /**
+     * Delete the groupMember by id.
+     *
+     * @param id the id of the entity.
+     * @return a Mono to signal the deletion
+     */
+    public Mono<Void> delete(Long id) {
+        log.debug("Request to delete GroupMember : {}", id);
+        return groupMemberRepository.deleteById(id);
+    }
+}
