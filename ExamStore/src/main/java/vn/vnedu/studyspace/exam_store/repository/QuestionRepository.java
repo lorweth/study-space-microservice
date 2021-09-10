a9442066f068bc53ca4e14e93b07e23adbea1748
witcher-vae <dhuuloc8818@gmail.com>
2021-09-10 20:49:40 +0700

    #3: Create some necessary queries

--- ExamStore/src/main/java/vn/vnedu/studyspace/exam_store/repository/QuestionRepository.java
+++ ExamStore/src/main/java/vn/vnedu/studyspace/exam_store/repository/QuestionRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 import vn.vnedu.studyspace.exam_store.domain.Question;
 
+import java.util.List;
+
 /**
  * Spring Data SQL repository for the Question entity.
  */
 @SuppressWarnings("unused")
 @Repository
-public interface QuestionRepository extends JpaRepository<Question, Long> {}
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+    List<Question> findByRepo_Id(Long id);
+}
