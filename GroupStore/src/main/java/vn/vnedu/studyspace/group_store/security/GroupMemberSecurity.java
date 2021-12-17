73b3ed821bc813ee7633d489089b8e7c81caeda4
witcher-vae <dhuuloc8818@gmail.com>
2021-12-17 07:42:10 +0700

    #5: Restricted API

---
+++ GroupStore/src/main/java/vn/vnedu/studyspace/group_store/security/GroupMemberSecurity.java
@@ -0,0 +1,81 @@
+package vn.vnedu.studyspace.group_store.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import vn.vnedu.studyspace.group_store.domain.GroupMember;
+import vn.vnedu.studyspace.group_store.repository.GroupMemberRepository;
+
+import java.util.Optional;
+
+@Component
+public class GroupMemberSecurity {
+
+    private final Logger log = LoggerFactory.getLogger(GroupMemberSecurity.class);
+
+    private final GroupMemberRepository groupMemberRepository;
+
+    public GroupMemberSecurity(GroupMemberRepository groupMemberRepository) {
+        this.groupMemberRepository = groupMemberRepository;
+    }
+
+    /**
+     * Check permission of current user login.
+     *
+     * @param groupId the id of the group.
+     * @param authority the authority.
+     * @return true if user has permission, false or else.
+     */
+    public boolean hasPermission(Long groupId, String authority){
+        Optional<String> userLogin = SecurityUtils.getCurrentUserLogin();
+        if(userLogin.isEmpty()){
+            log.debug("User not logged in");
+            return false;
+        }
+
+        Optional<GroupMember> member = groupMemberRepository.findByUserLoginAndGroupId(userLogin.get(), groupId);
+        if(member.isEmpty()){
+            log.debug("Member not exists");
+            return false;
+        }
+
+        switch (authority){
+            case "ADMIN":
+                return member.get().getRole() == 2;
+            case "MEMBER":
+                return member.get().getRole() >= 1; // Because Admin can do anything
+            case "WAITING":
+                return member.get().getRole() == 0;
+            default:
+                return false;
+        }
+    }
+
+    /**
+     * Check permission on group of current user login.
+     *
+     * @param memberId id of the member (to get the group id).
+     * @param authority the authority
+     * @return true if user has permission, false or else.
+     */
+    public boolean hasPermissionOnGroupOfMember(Long memberId, String authority) {
+        Optional<GroupMember> member = groupMemberRepository.findById(memberId);
+        if (member.isEmpty()){
+            log.debug("Member {} not exists", memberId);
+            return false;
+        }
+        Long groupId = member.get().getGroup().getId();
+        return hasPermission(groupId, authority);
+    }
+
+    public boolean notExists(Long groupId) {
+        Optional<String> userLogin = SecurityUtils.getCurrentUserLogin();
+        if(userLogin.isEmpty()){
+            log.debug("User not logged in");
+            return false;
+        }
+
+        Optional<GroupMember> member = groupMemberRepository.findByUserLoginAndGroupId(userLogin.get(), groupId);
+        return member.isEmpty();
+    }
+}
